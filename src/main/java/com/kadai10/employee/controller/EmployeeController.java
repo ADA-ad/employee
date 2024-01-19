fcbeb69e9b3787758a1d6b62faf69f926d37ef6e
ADA <tracyshi911@gmail.com>
2024-01-19 19:14:10 +0900

    5行目と11行目を変更し、application.propertiesのurlの記述と一致する。

--- src/main/java/com/kadai10/employee/controller/EmployeeController.java
+++ src/main/java/com/kadai10/employee/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.kadai10.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -22,10 +23,11 @@ public class EmployeeController {
      * コントローラのコンストラクタ。
      * @param employeeService ユーザーサービスのインスタンス
      */
-    @Autowired
+
     public  EmployeeController(EmployeeService employeeService) {
         this.employeeService =employeeService;
     }
+    @RequestMapping
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
