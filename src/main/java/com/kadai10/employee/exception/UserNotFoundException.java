a6a04e535a98e0babb479c88f10971cb65c1dc03
ADA <tracyshi911@gmail.com>
2024-01-23 19:22:18 +0900

    コメントを簡潔に修正する。

--- src/main/java/com/kadai10/employee/exception/UserNotFoundException.java
+++ src/main/java/com/kadai10/employee/exception/UserNotFoundException.java
@@ -4,11 +4,7 @@ package com.kadai10.employee.exception;
  * ユーザーが見つからない場合にスローされる例外クラスです。ユーザーが特定の条件を満たさない場合や、IDに対応するユーザーが存在しない場合に使用されます。
  */
 public class UserNotFoundException extends RuntimeException {
-    /**
-     * ユーザーが見つからない場合にスローされる例外クラス。 この例外は、指定されたユーザーがデータベースやリポジトリで見つからない場合にスローされます
-     *
-     * @param message 例外メッセージ
-     */
+    
     public UserNotFoundException(final String message) {
         super(message);
     }
