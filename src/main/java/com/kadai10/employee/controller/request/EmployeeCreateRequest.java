7082a32d5f7f01b4e80072589c92a3211a547a9f
ADA <tracyshi911@gmail.com>
2024-01-27 14:37:52 +0900

    EmployeeCreateRequestのコメントを修正し、インデントを整える。

--- src/main/java/com/kadai10/employee/controller/request/EmployeeCreateRequest.java
+++ src/main/java/com/kadai10/employee/controller/request/EmployeeCreateRequest.java
@@ -1,26 +1,16 @@
 package com.kadai10.employee.controller.request;
 
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 /**
- * ユーザー情報のリクエストを表すクラスです. このクラスは新しいユーザーの作成の要求を受け取ります。
+ * ユーザー情報のリクエストを表すレコードクラスです. このクラスは新しいユーザーの作成の要求を受け取ります。
  * @param name  ユーザーの名前を表すフィールド。
  * @param age  ユーザーの年齢を表すフィールド
  * @param address  ユーザーの住所を表すフィールド
  */
 
 public record EmployeeCreateRequest (
-        /**
-         * EmployeeCreateRequest オブジェクトを生成するためのコンストラクタ.
-         *
-         * @param name ユーザーの名前
-         * @param age ユーザーの年齢
-         * @param address ユーザーの住所
-         *
-         */
+
         @NotBlank (message = "名前を入力してください")
         @Size(max = 8)
         String name,
@@ -33,8 +23,8 @@ public record EmployeeCreateRequest (
         @NotBlank(message = "住所を入力してください")
         @Size(max = 50)
         String address
-
-) {
+)
+{
 
 
 
